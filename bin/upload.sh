e7441536f2c2f06394dd6315a8250379d68b053e
xiaochun <xchun90@163.com>
2024-05-04 08:25:47 +0800

    Update upload.sh

--- bin/upload.sh
+++ bin/upload.sh
@@ -5,8 +5,10 @@ templates=("template-simple" "template-sheshui" "template-sheshui-2018" "templat
 # 循环处理每个模板
 for template in "${templates[@]}"
 do
+    cd $template
     echo "正在压缩 $template ..."
-    zip -9 -r "zip/attachment/template/${template}.zip" "$template/*" > /dev/null
+    zip -9 -r "../zip/attachment/template/${template}.zip" "$template/*" > /dev/null
+    cd ..
 done
 
 echo "所有模板已成功压缩。"
