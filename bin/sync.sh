978e3beb9c48a67cec34f153ff6d77d03de9672f
xiaochun <xchun90@163.com>
2024-06-03 12:40:21 +0800

    Add

--- bin/sync.sh
+++ bin/sync.sh
@@ -2,6 +2,6 @@ syncFolder=${4}
 #upload
 bash -e bin/download-ossutil.sh || true
 cd ${syncFolder}
-../ossutil config -i ${1} -k ${2} -e oss-accelerate.aliyuncs.com
+../ossutil config -i ${1} -k ${2} -e oss-cn-chengdu.aliyuncs.com
 ../ossutil cp --recursive -f . oss://${3}
 
